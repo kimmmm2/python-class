e5f99a3403861e056fe6c5bf1f59727d79e06740
kimmmm2 <teddkimani@icloud.com>
2025-07-16 16:41:34 +0300

    final commit

---
+++ Intro.py
@@ -0,0 +1,5 @@
+#Print is a method used to display a value
+print("Tedd")
+print("I am learning Python Programming")
+print(100)
+print(5 + 6)
---
+++ variables.py
@@ -0,0 +1,15 @@
+#variables
+number = 10
+num2 = 6
+firstname = "John"
+lastname = "Doe"
+language = 'Python'
+
+
+print(number)
+print(num2)
+print(firstname)
+print(number + num2)
+print("My first name is", firstname)
+print("My Full name is", firstname,lastname)
+print(language ,"is a Web Programmin language")
